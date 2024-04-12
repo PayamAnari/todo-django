4d343ea32e4c7a5af4693a858ef628ea5310e540
PayamAnari <anari.p62@gmail.com>
2024-04-12 12:48:06 +0200

    Add Auth_user_model to settings

--- authentication/models.py
+++ authentication/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from helpers.models import TrackingModel
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
 
-
-class User(TrackingModel):
+class User(AbstractBaseUser,PermissionsMixin,TrackingModel):
     pass
 
 
