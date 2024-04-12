0c082f407270596a6e889febca5ff575b42ce1e0
PayamAnari <anari.p62@gmail.com>
2024-04-12 13:17:42 +0200

    Add is_active

--- authentication/models.py
+++ authentication/models.py
@@ -61,6 +61,14 @@ class User(AbstractBaseUser,PermissionsMixin,TrackingModel):
         _('staff status'),
         default=False,
         help_text=_('Designates whether the user can log into this admin site.'),
-        
-    )
 
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    
