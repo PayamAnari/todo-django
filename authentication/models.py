2c35157ef8ec180c9d3e67563ebb933a9d8846cb
PayamAnari <anari.p62@gmail.com>
2024-04-12 13:09:18 +0200

    Create user def

--- authentication/models.py
+++ authentication/models.py
@@ -23,6 +23,12 @@ class MyUserManager(UserManager):
         user.save(using=self._db)
         return user
 
+    def create_user(self, username, email, password=None, **extra_fields):
+          
+          extra_fields.setdefault('is_staff', False)
+          extra_fields.setdefault('is_superuser', False)
+          return self._create_user(username, email, password, **extra_fields)
+
 class User(AbstractBaseUser,PermissionsMixin,TrackingModel):
     pass
 
