078ca7fd9681e4270afa7d180b5fb9dfce7b69ff
PayamAnari <anari.p62@gmail.com>
2024-04-12 14:24:09 +0200

    Add postgresql and migration

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'todolistapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER': 'postgres',
+        'PASSWORD': '2219499',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
