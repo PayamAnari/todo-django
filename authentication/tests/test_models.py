40b588207db7360359f29881257a62f418d151f2
PayamAnari <anari.p62@gmail.com>
2024-04-12 16:24:01 +0200

    Modify test.models

---
+++ authentication/tests/test_models.py
@@ -0,0 +1,11 @@
+from rest_framework.test import APITestCase
+from authentication.models import User
+
+class TestModel(APITestCase):
+
+    def test_creates_user(self):
+        user = User.objects.create_user(
+            'username','example@gmail.com', 'password123'
+        )
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.email,User)
