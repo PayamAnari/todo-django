2bcc6ddafe59b7ca40d6412928bb74559dd337a2
PayamAnari <anari.p62@gmail.com>
2024-04-12 12:41:18 +0200

    Create user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from helpers.models import TrackingModel
 
-# Create your models here.
+
+class User(TrackingModel):
+    pass
+
+
--- helpers/models.py
+++ helpers/models.py
@@ -6,4 +6,5 @@ class TrackingModel(models.Model):
     updated_at=models.DateTimeField(auto_now=True)
 
     class Meta:
+        abstract=True
         ordering=('created_at', 'updated_at')
