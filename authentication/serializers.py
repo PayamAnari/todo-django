facb714f3aa9cccd9d119416e7c009a7b417c5a7
PayamAnari <anari.p62@gmail.com>
2024-04-13 15:39:32 +0200

    Create register srializers def

---
+++ authentication/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from authentication.models import User
+
+
+class RegisterSerializers(serializers.modelSerializers):
+
+    password = serializers.CharField(max_length=128, min_lenght=6, write_only=True)
+
+
+    class Meta():
+      model = User
+      fields = ['username', 'first_name', 'last_name', 'email', 'password']
