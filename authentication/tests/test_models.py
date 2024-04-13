080ab31c0d9db49a9f7413ef7257bdf56dca5163
PayamAnari <anari.p62@gmail.com>
2024-04-13 15:24:51 +0200

    Add error rais is_superuser test

--- authentication/tests/test_models.py
+++ authentication/tests/test_models.py
@@ -28,6 +28,10 @@ class TestModel(APITestCase):
     def test_cant_create_super_user_with_no_is_staff_status(self):
         with self.assertRaisesMessage(ValueError, 'Superuser must have is _staff=True.'):
             User.objects.create_superuser(username='payam', email='payam@gmail.com', password='password123!@', is_staff=False)
+    
+    def test_cant_create_super_user_with_no_is_superuser_status(self):
+        with self.assertRaisesMessage(ValueError, 'Superuser must have is_superuser=True.'):
+            User.objects.create_superuser(username='payam',email='payam@gmail.com', password='password123!@', is_superuser=False)
 
     def test_creates_super_user(self):
         user = User.objects.create_superuser(
