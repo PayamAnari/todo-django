b5e3e45e7a06458f86f39a4f5fa9bc551059d060
PayamAnari <anari.p62@gmail.com>
2024-04-13 15:42:57 +0200

    Create user def

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -7,6 +7,10 @@ class RegisterSerializers(serializers.modelSerializers):
     password = serializers.CharField(max_length=128, min_lenght=6, write_only=True)
 
 
-    class Meta():
+    class Meta:
       model = User
       fields = ['username', 'first_name', 'last_name', 'email', 'password']
+
+    
+    def create(self, validated_data):
+      return User.objects.create_user(**validated_data)
