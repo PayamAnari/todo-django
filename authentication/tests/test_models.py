436783bad4196da95ab278c10e829667655328e8
PayamAnari <anari.p62@gmail.com>
2024-04-13 15:23:23 +0200

    Add error raise is_staff test

--- authentication/tests/test_models.py
+++ authentication/tests/test_models.py
@@ -24,6 +24,10 @@ class TestModel(APITestCase):
     def test_raises_error_with_message_when_no_email_is_supplied(self):
         with self.assertRaisesMessage(ValueError, 'The given email must be set'):
             User.objects.create_user(username='payam', email='', password='password123!@')
+    
+    def test_cant_create_super_user_with_no_is_staff_status(self):
+        with self.assertRaisesMessage(ValueError, 'Superuser must have is _staff=True.'):
+            User.objects.create_superuser(username='payam', email='payam@gmail.com', password='password123!@', is_staff=False)
 
     def test_creates_super_user(self):
         user = User.objects.create_superuser(
