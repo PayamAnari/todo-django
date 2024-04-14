56a9fa59119923b070cb8003c7c4bbf7da59693f
PayamAnari <anari.p62@gmail.com>
2024-04-14 11:14:46 +0200

    Add user path

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('users', views.UsersAPIView.as_view(), name='users'),
     path('users/<int:pk>', views.UserAPIView.as_view(), name='user'),
     path('login', views.LoginAPIView.as_view(), name='login'),
+    path('user', views.AuthUserAPIView.as_view(), name='user'),
 ]
