50197de7ab3b22b0a792a07b8fd4f6ed4b027d05
PayamAnari <anari.p62@gmail.com>
2024-04-14 11:35:44 +0200

    Modify jwt authentication

--- authentication/jwt.py
+++ authentication/jwt.py
@@ -14,4 +14,27 @@ class JWTAuthentication(BaseAuthentication):
         auth_data = auth_header.decode('utf_8')
         auth_token = auth_data(' ')
 
-        
+        if len(auth_token) != 2:
+            raise exceptions.AuthenticationFailed('Token is invalid')
+
+        token = auth_token[1]
+
+        try:
+           payload = jwt.decode(
+              token, settings.SECRET_KEY, algorithms = 'HS256')
+           username = payload['username']
+           user = User.objects.get(username=username)
+           return (user, token)
+
+        except jwt.ExpiredSignatureError as ex:
+            raise exceptions.AuthenticationFailed('Token is expired, login again')
+
+        except jwt.DecodeError as ex:
+            raise exceptions.AuthenticationFailed('Token is invalid')
+
+        except User.DoesNotExist as ex:
+            raise exceptions.AuthenticationFailed('User not found')
+
+        return super().authenticate(request)
+
+
