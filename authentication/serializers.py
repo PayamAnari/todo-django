05e680dc92a47266cef4ad677f641c8a223c018c
PayamAnari <anari.p62@gmail.com>
2024-04-14 13:28:07 +0200

    Create updateuser serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -35,3 +35,19 @@ class LoginSerializer(serializers.ModelSerializer):
       fields = ['email', 'username', 'password', 'token']
 
       read_only_fields = ['token']
+
+
+class UpdateUserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_active', 'is_superuser']
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.is_active = validated_data.get('is_active', instance.is_active)
+        instance.is_superuser = validated_data.get('is_superuser', instance.is_superuser)
+        instance.save()
+        return instance
