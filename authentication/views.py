826301f35be5cdd033b6d039c4e884aa3a8ab94d
PayamAnari <anari.p62@gmail.com>
2024-04-15 16:47:23 +0200

    Modify todo model

--- authentication/views.py
+++ authentication/views.py
@@ -2,47 +2,57 @@ from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
-from authentication.serializers import RegisterSerializers, GetUsersSerializers, LoginSerializer, GetUserSerializers, UpdateUserSerializers
+from authentication.serializers import (
+    RegisterSerializers,
+    GetUsersSerializers,
+    LoginSerializer,
+    GetUserSerializers,
+    UpdateUserSerializers,
+)
 from django.contrib.auth import authenticate, logout
 from authentication.models import User
 from django.core.exceptions import ObjectDoesNotExist
 
 
-
 class AuthUserAPIView(GenericAPIView):
-      
-      permission_classes = [permissions.IsAuthenticated]
-      
-      def get(self, request):
-          user = request.user
-          serializer = RegisterSerializers(user)
-          return Response({'user':serializer.data}, status=status.HTTP_200_OK)
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = RegisterSerializers(user)
+        return Response({"user": serializer.data}, status=status.HTTP_200_OK)
+
 
 class RegisterAPIView(GenericAPIView):
-     
-     authentication_classes = []
-     
-     serializer_class = RegisterSerializers
 
-     def post(self, request):
-          serializer = self.serializer_class(data=request.data)
-          if serializer.is_valid():
-              serializer.save()
-              return Response({"message": 'User created successfully!', "data": serializer.data}, status=status.HTTP_201_CREATED)
-          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    authentication_classes = []
+
+    serializer_class = RegisterSerializers
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User created successfully!", "data": serializer.data},
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UsersAPIView(GenericAPIView):
-   
-   serializer_class = GetUsersSerializers
 
-   def get(self, request):
-          users = User.objects.all()
-          serializer = self.serializer_class(users, many=True)
-          return Response(serializer.data, status=status.HTTP_200_OK)
+    serializer_class = GetUsersSerializers
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = self.serializer_class(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class UserAPIView(GenericAPIView):
-   
+
     serializer_class = GetUserSerializers
 
     def get(self, request, pk):
@@ -51,50 +61,56 @@ class UserAPIView(GenericAPIView):
             serializer = self.serializer_class(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except ObjectDoesNotExist:
-            return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"message": "User not found"}, status=status.HTTP_404_NOT_FOUND
+            )
 
 
 class LoginAPIView(GenericAPIView):
-   
-    authentication_classes = []
-    
-    serializer_class = LoginSerializer
-    
-    def post(self, request):
-        email = request.data.get('email', None)
-        password = request.data.get('password', None)
 
-        user = authenticate(username= email, password= password)
+    authentication_classes = []
+
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        email = request.data.get("email", None)
+        password = request.data.get("password", None)
+
+        user = authenticate(username=email, password=password)
 
         if user:
-          serializer = self.serializer_class(user)
+            serializer = self.serializer_class(user)
 
-          return Response({'message': 'User logged in successfully!', 'data': serializer.data})
+            return Response(
+                {"message": "User logged in successfully!", "data": serializer.data}
+            )
 
-        return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
-
+        return Response(
+            {"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+        )
 
 
 class LogOutAPIView(GenericAPIView):
-    
+
     permission_classes = [permissions.IsAuthenticated]
-    
+
     def post(self, request):
         logout(request)
-        return Response({'message': 'User logged out successfully!'}, status=status.HTTP_200_OK)
-
+        return Response(
+            {"message": "User logged out successfully!"}, status=status.HTTP_200_OK
+        )
 
 
 class DeleteUserAPIView(GenericAPIView):
 
-     permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
-     def delete(self, request, pk):
-         user = User.objects.get(id=pk)
-         user.delete()
-         return Response({'message': 'User deleted successfully!'}, status=status.HTTP_200_OK)
-
+    def delete(self, request, pk):
+        user = User.objects.get(id=pk)
+        user.delete()
+        return Response(
+            {"message": "User deleted successfully!"}, status=status.HTTP_200_OK
+        )
 
 
 class UpdateUserAPIView(GenericAPIView):
@@ -106,7 +122,13 @@ class UpdateUserAPIView(GenericAPIView):
         serializer = self.get_serializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': 'User information updated successfully', 'data': serializer.data}, status=status.HTTP_200_OK)
+            return Response(
+                {
+                    "message": "User information updated successfully",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, *args, **kwargs):
--- todos/models.py
+++ todos/models.py
@@ -3,13 +3,13 @@ from helpers.models import TrackingModel
 from authentication.models import User
 
 
-
-
 class Todo(TrackingModel):
 
     title = models.CharField(max_length=255)
     description = models.TextField()
+    due_date = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=255)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
