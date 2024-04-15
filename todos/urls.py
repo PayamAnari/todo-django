2b5a2fc470c430203458882a33e6965444f38377
PayamAnari <anari.p62@gmail.com>
2024-04-15 17:20:48 +0200

    Add list todo path

--- todos/urls.py
+++ todos/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from todos.views import CreateTodoAPIView
+from todos.views import CreateTodoAPIView, TodoListAPIView
 
 
-urlpatterns = [path("create", CreateTodoAPIView.as_view(), name="create_todo")]
+urlpatterns = [path("create", CreateTodoAPIView.as_view(), name="create_todo")
+    path("list", TodoListAPIView.as_view(), name="list_todo")
+]
