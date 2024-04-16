8c7e9791f0271801463c492129c82dbf38550029
PayamAnari <anari.p62@gmail.com>
2024-04-16 16:26:48 +0200

    Add schema views to urls

--- todolistapi/urls.py
+++ todolistapi/urls.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Todo List API",
+        default_version="v1",
+        description="API for managing todo items",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="anari.p62@gmail.com", version="1.0.0"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[
+        permissions.AllowAny,
+    ],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
