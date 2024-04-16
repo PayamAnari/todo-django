02c40975b44e92c8c6ca7f081173a6809997657b
PayamAnari <anari.p62@gmail.com>
2024-04-16 11:33:12 +0200

    Create test todo

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.http import response
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from todos.models import Todo
 
-# Create your tests here.
+
+class TodosAPITestCase(APITestCase):
+
+    def create_todo(self):
+        sample_todo = {
+            "title": "title test",
+            "description": "description test",
+            "priority": "H",
+            "is_completed": False,
+            "due_date": "2022-12-12",
+        }
+        response = self.client.post(reverse("todos"), sample_todo)
+        return response
