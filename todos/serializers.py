e73c93a0ee97a656697818f71df511619c982448
Payam Anari <anari.p62@gmail.com>
2024-04-17 10:31:58 +0200

    Update serializers.py

--- todos/serializers.py
+++ todos/serializers.py
@@ -3,7 +3,7 @@ from todos.models import Todo
 from authentication.serializers import GetUserDetailInTodo
 
 
-class TodoSerializer(ModelSerializer):
+class TodoRetrieveSerializer(ModelSerializer):
 
     owner = GetUserDetailInTodo(read_only=True)
 
@@ -17,4 +17,17 @@ class TodoSerializer(ModelSerializer):
             "due_date",
             "description",
             "owner",
+        ]
+
+
+class TodoCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = [
+            "id",
+            "title",
+            "is_completed",
+            "priority",
+            "due_date",
+            "description",
         ]
